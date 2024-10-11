14122123aad05aab8370885aba5a078221dd611b
gianlucaromeo <gianlucaromeo@outlook.com>
2024-10-11 17:01:08 +0300

    updated mock data

--- finnish_survival/lib/mock_data.dart
+++ finnish_survival/lib/mock_data.dart
@@ -1,28 +1,69 @@
-final greetings = {
-  "Hello": {
-    "formal": ["Moi", "Moikka", "Hei", "Päivää"],
-    "informal": ["Moro", "Moi", "Heippa", "Heihei"],
+final topics = {
+  "Greetings": {
+    "id": "greetings",
+    "learn": {
+      "Hello": {
+        "id": "hello",
+        "formal": ["Moi", "Moikka", "Hei", "Päivää"],
+        "informal": ["Moro", "Moi", "Heippa", "Heihei"],
+      },
+      "Goodbye": {
+        "id": "goodbye",
+        "formal": ["Näkemiin", "Hyvästi", "Hei hei"],
+        "informal": ["Moikka", "Heippa", "Heihei"],
+      },
+      "Thank you": {
+        "id": "thank_you",
+        "formal": ["Kiitos", "Suuret kiitokset", "Paljon ki"],
+        "informal": ["Kiitti", "Kiitos", "Kiitos paljon"],
+      },
+      "You're welcome": {
+        "id": "youre_welcome",
+        "formal": ["Ole hyvä", "Ei kestä", "Eipä kestä"],
+        "informal": ["Eipä kestä", "Ei kestä", "Ole hyvä"],
+      },
+      "Excuse me": {
+        "id": "excuse_me",
+        "formal": ["Anteeksi", "Anteeksi, että häiritsen"],
+        "informal": ["Anteeksi", "Anteeksi, että häiritsen"],
+      },
+      "I'm sorry": {
+        "id": "im_sorry",
+        "formal": ["Olen pahoillani", "Anteeksi"],
+        "informal": ["Olen pahoillani", "Anteeksi"],
+      },
+    },
+    "practice": {
+      "1": {
+        "id": "practice_1",
+        "question": "How do you say 'Hello' in Finnish?",
+        "answers": ["Moi", "Anteeksi", "Kiitos", "Minä"],
+        "correct": "Moi",
+      },
+      "2": {
+        "id": "practice_2",
+        "question": "How do you say 'Goodbye' in Finnish?",
+        "answers": ["Näkemiin", "Hei", "Moi", "Heippa"],
+        "correct": "Näkemiin",
+      },
+    }
   },
-  "Goodbye": {
-    "formal": ["Näkemiin", "Hyvästi", "Hei hei"],
-    "informal": ["Moikka", "Heippa", "Heihei"],
+  "Family": {
+    "id": "family",
   },
-  "Thank you": {
-    "formal": ["Kiitos", "Suuret kiitokset", "Paljon ki"],
-    "informal": ["Kiitti", "Kiitos", "Kiitos paljon"],
+  "Numbers": {
+    "id": "numbers",
   },
-  "You're welcome": {
-    "formal": ["Ole hyvä", "Ei kestä", "Eipä kestä"],
-    "informal": ["Eipä kestä", "Ei kestä", "Ole hyvä"],
+  "Days of the Week": {
+    "id": "days_of_the_week",
   },
-  "Excuse me": {
-    "formal": ["Anteeksi", "Anteeksi, että häiritsen"],
-    "informal": ["Anteeksi", "Anteeksi, että häiritsen"],
+  "Verb To Be": {
+    "id": "verb_to_be",
   },
-  "I'm sorry": {
-    "formal": ["Olen pahoillani", "Anteeksi"],
-    "informal": ["Olen pahoillani", "Anteeksi"],
+  "Verb To Have": {
+    "id": "verb_to_have",
+  },
+  "Verb To Do": {
+    "id": "verb_to_do",
   },
 };
-
-final userGreetingsFavorites = ["Hello", "You're welcome"];
